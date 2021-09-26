af2231ae1efcab55c8304413ddd4153bbcefd1c6
Danny Shen <shenfive@hotmail.com>
2021-09-26 11:23:02 +0800

    Initial Commit

---
+++ a815push/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  a815push
+//
+//  Created by 申潤五 on 2021/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
